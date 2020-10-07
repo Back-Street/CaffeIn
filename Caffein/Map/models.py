51062a1ac3ce66bf702d2475735ed234648f71f4
dongyeopca <ckehdduq95@gmail.com>
2020-10-08 00:33:37 +0900

    맵 마커표시/마커중심이동/내위치/검색/모달 추가

--- Caffein/Map/models.py
+++ Caffein/Map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cafe(models.Model):
+    name = models.CharField('카페이름', max_length=20)
+    x = models.FloatField('경도')
+    y = models.FloatField('위도')
+    image = models.ImageField('카페사진',blank=True)
+
+    def __str__(self):
+        return self.name
--- Caffein/Map/urls.py
+++ Caffein/Map/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path,re_path
+from django.urls import path
 from Map.views import map_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name:'Map'
 urlpatterns=[
-    path('',map_view, name='Map'),
-]
+    path('',map_view, name='map'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
