318ca65af536ca8cbc50acf8155bc78cef92f808
mattew8 <mattew4483@likelion.org>
2020-10-30 23:20:58 +0900

    [add] 템플릿에 model object값 전달

--- Caffein/store/views.py
+++ Caffein/store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Store 
+from .models import Store,Menu 
 # Create your views here.
 
 class StoreList(ListView):
